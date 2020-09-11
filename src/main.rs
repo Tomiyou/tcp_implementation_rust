c6b1372fa138b9679b1e1b904cdc216958c66c3d
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2020-09-11 17:49:57 +0200

    Now able to send packets.

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ struct Quad {
 
 fn main() -> io::Result<()> {
     let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
-    let tunnel = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("");
+    let tunnel = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("Error opening tun_tap tunnel");
     let mut buf = [0u8; 1504];
 
     loop {
         let bytes_read = tunnel.recv(&mut buf[..])?;
         // let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
         let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        // println!("Eth {:x?}", &buf[0..4]);
+        
         if eth_proto != 0x800 {
             // skip non IPv4 packets
             continue;
@@ -38,6 +40,7 @@ fn main() -> io::Result<()> {
             continue;
         }
 
+        // ip header size
         let iph_size = ip_header.slice().len();
 
         // parse TCP packet header
@@ -59,6 +62,6 @@ fn main() -> io::Result<()> {
             src: (ip_header.source_addr(), tcp_header.source_port()),
             dst: (ip_header.destination_addr(), tcp_header.destination_port()),
         }).or_default();
-        connection.accept(ip_header, tcp_header, &buf[datai..bytes_read]);
+        connection.accept(&tunnel, ip_header, tcp_header, &buf[datai..bytes_read])?;
     }
 }
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 pub struct Connection {}
 
 impl Default for Connection {
@@ -7,20 +9,73 @@ impl Default for Connection {
 }
 
 impl Connection {
+    fn write(
+        &mut self,
+        ip_header: etherparse::Ipv4Header,
+        tcp_header: etherparse::TcpHeader,
+        net_ifc: &tun_tap::Iface,
+    ) -> io::Result<usize> {
+        let mut buf = [0u8; 1504];
+        buf[2] = 0x8;
+
+        let bytes_written = {
+            let mut buf = &mut buf[4..];
+            ip_header
+                .write(&mut buf)
+                .expect("Unable to write IP packet to interface");
+            tcp_header
+                .write(&mut buf)
+                .expect("Unable to write TCP packet to interface");
+            1504 - buf.len()
+        };
+
+        net_ifc.send(&buf[..bytes_written])
+    }
+
     pub fn accept<'a>(
         &mut self,
+        net_ifc: &tun_tap::Iface,
         ip_header: etherparse::Ipv4HeaderSlice<'a>,
         tcp_header: etherparse::TcpHeaderSlice<'a>,
         data: &'a [u8],
-    ) {
-        println!(
-            "{}    {}:{} -> {}:{}      {} bytes",
-            ip_header.protocol(),
-            ip_header.source_addr(),
-            tcp_header.source_port(),
-            ip_header.destination_addr(),
+    ) -> io::Result<usize> {
+        if !tcp_header.syn() {
+            // only accept SYN packets at this point
+            return Ok(0)
+        }
+
+        let source = [
+            ip_header.source()[0],
+            ip_header.source()[1],
+            ip_header.source()[2],
+            ip_header.source()[3],
+        ];
+        let destination = [
+            ip_header.destination()[0],
+            ip_header.destination()[1],
+            ip_header.destination()[2],
+            ip_header.destination()[3],
+        ];
+
+        // create TCP packet
+        let mut syn_ack = etherparse::TcpHeader::new(
             tcp_header.destination_port(),
-            data.len()
+            tcp_header.source_port(),
+            0,
+            500,
         );
+        syn_ack.syn = true;
+        syn_ack.ack = true;
+
+        // create IP packet
+        let ipp = etherparse::Ipv4Header::new(
+            syn_ack.header_len(),
+            64,
+            etherparse::IpTrafficClass::Tcp,
+            destination,
+            source,
+        );
+
+        self.write(ipp, syn_ack, net_ifc)
     }
 }
